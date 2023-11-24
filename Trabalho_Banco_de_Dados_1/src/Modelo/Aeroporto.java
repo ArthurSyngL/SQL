28428674a66e3c7041d6295c17809a700a183d5d
ArthurSyngL <133982109+ArthurSyngL@users.noreply.github.com>
2023-11-23 21:02:40 -0300

    Atualizado

---
+++ Trabalho_Banco_de_Dados_1/src/Modelo/Aeroporto.java
@@ -0,0 +1,32 @@
+package Modelo;
+
+public class Aeroporto {
+    private int codigo;
+    private String nome;
+    private String localizacao;
+
+    public int getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(int codigo) {
+        this.codigo = codigo;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getLocalizacao() {
+        return localizacao;
+    }
+
+    public void setLocalizacao(String localizacao) {
+        this.localizacao = localizacao;
+    }
+
+}
